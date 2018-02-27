ac995e236b92a184eacb323ecb2a65f208a0453c
sugino <github@sugi.cc>
2018-02-27 14:53:33 +0900

    ParticleSystem.Particleの構造を調べた

---
+++ Assets/ParticleSystem-GPU/Scripts/ParticleSystemStudy.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using UnityEngine;
+
+public class ParticleSystemStudy : MonoBehaviour
+{
+    public Particle editable;
+    public Particle[] particles;
+
+    public ComputeShader computeBridge;
+    ComputeBuffer particleDataBuffer;
+
+    new ParticleSystem particleSystem;
+    ParticleSystem.Particle[] particleData;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleData = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+        particles = new Particle[particleData.Length];
+
+        var size = Marshal.SizeOf(typeof(ParticleSystem.Particle));
+        Debug.Log(size);
+        size = Marshal.SizeOf(typeof(Particle));
+        Debug.Log(size);
+
+        size = 0;
+        var particle = new ParticleSystem.Particle();
+        Debug.Log(Marshal.SizeOf(particle.startSize));
+
+        var ps = typeof(ParticleSystem.Particle).GetProperties();
+
+        foreach (var p in ps)
+        {
+            if (p.GetCustomAttributes(false).Length < 1)
+                if (p.CanWrite)
+                {
+                    //Debug.Log(p);
+                    //Debug.Log(p.PropertyType);
+                    size += Marshal.SizeOf(p.PropertyType);
+                }
+        }
+        Debug.Log(size);
+
+        particleDataBuffer = new ComputeBuffer(10, Marshal.SizeOf(typeof(ParticleSystem.Particle)));
+    }
+
+    private void OnDestroy()
+    {
+        particleDataBuffer.Release();
+    }
+
+    private void Update()
+    {
+
+        var num = particleSystem.GetParticles(particleData);
+        particleDataBuffer.SetData(particleData, 0, 0, num);
+        var kernel = computeBridge.FindKernel("bridge");
+        computeBridge.SetBuffer(kernel, "_ParticlesOUT", particleDataBuffer);
+        computeBridge.Dispatch(kernel, 1, 1, 1);
+        particleDataBuffer.GetData(particleData, 0, 0, num);
+
+        for (var i = 0; i < num; i++)
+            particles[i].SetData(particleData[i]);
+
+    }
+
+    //for inspector
+    [System.Serializable]
+    public struct Particle
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+        public Vector3 animatedVelocity;
+        public Vector3 totalVelocity;
+        public float remainingLifetime;
+        public float startLifetime;
+        public Vector3 startSize3D;
+        public Vector3 axisOfRotation;
+        public float rotation;
+        public Vector3 rotation3D;
+        public float angularVelocity;
+        public Vector3 angularVelocity3D;
+        public Color32 startColor;
+        public uint randomSeed;
+        public float startSize;
+
+        public void SetData(ParticleSystem.Particle source)
+        {
+            position = source.position;
+            velocity = source.velocity;
+            animatedVelocity = source.animatedVelocity;
+            totalVelocity = source.totalVelocity;
+            remainingLifetime = source.remainingLifetime;
+            startLifetime = source.startLifetime;
+            startSize3D = source.startSize3D;
+            axisOfRotation = source.axisOfRotation;
+            rotation = source.rotation;
+            rotation3D = source.rotation3D;
+            angularVelocity = source.angularVelocity;
+            angularVelocity3D = source.angularVelocity3D;
+            startColor = source.startColor;
+            randomSeed = source.randomSeed;
+            startSize = source.startSize;
+        }
+
+        public ParticleSystem.Particle GetUnityParticle()
+        {
+            var particle = new ParticleSystem.Particle();
+            particle.position = position;
+            particle.velocity = velocity;
+            //particle.animatedVelocity = animatedVelocity;
+            //particle.totalVelocity = totalVelocity;
+            particle.remainingLifetime = remainingLifetime;
+            particle.startLifetime = startLifetime;
+            particle.startSize3D = startSize3D;
+            particle.axisOfRotation = axisOfRotation;
+            particle.rotation = rotation;
+            particle.rotation3D = rotation3D;
+            particle.angularVelocity = angularVelocity;
+            particle.angularVelocity3D = angularVelocity3D;
+            particle.startColor = startColor;
+            particle.randomSeed = randomSeed;
+            particle.startSize = startSize;
+            return particle;
+        }
+    }
+}
--- Assets/ParticleSystem-GPU/Shaders/ParticleSystem.Particle.cginc
+++ Assets/ParticleSystem-GPU/Shaders/ParticleSystem.Particle.cginc
@@ -5,20 +5,30 @@ struct Particle
     float3 position;
     float3 velocity;
     float3 animatedVelocity;
-    float3 totalVelocity;
+    float3 totalVelocity; //velocityとanimatedの足し算。自動的
+    float3 axisOfRotation;
+    float3 rotation3D;
+    float3 angularVelocity3D;
+    float3 startSize3D;
+    uint startColor; //Color32の代わり 0xFFFFFFFFで設定する(ABRGの順)
+    uint randomSeed;
     float remainingLifetime;
     float startLifetime;
-    float3 startSize3D;
-    float3 axisOfRotation;
-    float rotation;
-    float3 rotation3D;
-    float angularVelocity;
-    float3 angularVelocity3D;
-    uint startColor; //Color32の代わり
-    uint randomSeed;
-//  float startSize;
+    float rotation; //rotation3Dから、自動的に設定されるぽい
+    float angularVelocity; //angularVelociry3Dから、自動設定
+    //float startSize;
 // sizeof(ParticleSystem.Particle) = 120なので、floatかint、1個分多いので、とりあえずstartSizeをコメントアウトしてみた
-// この並び順は、UnityEngine.ParticleSystem.ParticleのメタデータのStructの逆順。
+// この並び順は、ParticleUpdate.compute bridge-kernelで調査した
 };
+
+
+uint Particle_ColorToUint(half4 col)
+{
+    uint col32 = ((uint) (col.r * 0xFF))
+        + ((uint) (col.g * 0xFF) << 8)
+        + ((uint) (col.b * 0xFF) << 16)
+        + ((uint) (col.a * 0xFF) << 24);
+    return col32;
+}
 
 #endif
