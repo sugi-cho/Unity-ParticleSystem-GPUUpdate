474961274c966e14138005776ffde47bafe3bf68
sugino <github@sugi.cc>
2018-03-01 15:32:10 +0900

    add mesh data writer.shader

---
+++ Assets/ParticleSystem-GPU/Scripts/MeshDataTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MeshDataTest : MonoBehaviour {
+
+    public Material bufferWriter;
+    Mesh targetMesh;
+
+    ComputeBuffer vDataBuffer;
+    [Header("for debug")]
+    [SerializeField] vData[] dataArray;
+
+	// Use this for initialization
+	void Start () {
+        targetMesh = GetComponent<MeshFilter>().sharedMesh;
+        var vCount = targetMesh.vertexCount;
+        dataArray = new vData[vCount];
+        vDataBuffer = new ComputeBuffer(vCount, Marshal.SizeOf(typeof(vData)));
+	}
+
+    private void OnDestroy()
+    {
+        vDataBuffer.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Graphics.SetRandomWriteTarget(1, vDataBuffer);
+        bufferWriter.SetPass(0);
+        Graphics.DrawMeshNow(targetMesh, transform.localToWorldMatrix);
+        Graphics.ClearRandomWriteTargets();
+
+        vDataBuffer.GetData(dataArray);
+	}
+
+    [System.Serializable]
+    public struct vData
+    {
+        public Vector3 pos;
+        public Vector3 normal;
+        public Vector2 uv;
+    }
+}
---
+++ Assets/ParticleSystem-GPU/Shaders/MeshDataWriter.shader
@@ -0,0 +1,42 @@
+﻿Shader "Hidden/MeshDataWriter"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 4.5
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+				uint vid : SV_VertexID;
+			};
+			struct vData {
+				float3 pos;
+				float3 normal;
+				float2 uv;
+			};
+			RWStructuredBuffer<vData> _VDataOut : register(u1);
+
+			void vert(appdata v)
+			{
+				vData data = (vData)0;
+				data.pos = mul(unity_ObjectToWorld, v.vertex);
+				data.normal = UnityObjectToWorldNormal(v.normal);
+				data.uv = v.uv;
+				_VDataOut[v.vid] = data;
+			}
+
+			void frag(){}
+
+			ENDCG
+		}
+	}
+}
